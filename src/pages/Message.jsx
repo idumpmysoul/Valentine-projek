79eb2e37e25d4349128f51b0eac2f7b18193728c
idumpmysoul <abedzebua777@gmail.com>
2024-02-17 13:19:51 +0700

    update Message page

--- src/pages/Message.jsx
+++ src/pages/Message.jsx
@@ -1,28 +1,24 @@
 import LoveBackground from "../assets/love.svg";
 import Heart from "../assets/Heart.png";
-
+import Bubble from "../assets/buletbulet.png";
 
 
 
 const Message = () => {
     return (
         <>
-            <div className="w-full h-auto md:max-w-[1247px] py-10 px-[20px] md:px-[50px] lg:px-[166.5px] bg-white rounded-[20px] text-center shadow-xl shadow-shadowcard">
-            <div className="flex justify-end items-center h-full ">
+    <div className="overflow-hidden w-full h-auto md:max-w-[1247px] py-10 px-[20px] md:px-[50px] lg:px-[166.5px] bg-white rounded-[20px] text-center -rotate-2 shadow-xl shadow-shadowcard">
+    <div className="overflow-hidden flex justify-end items-center h-full ">
     <img src={Heart} className="inline-block" />
     <img src={Heart} className="inline-block" />
     
 </div>
-
-
-
-                
-                <br />
+<br />
                 
                 {/* w-[330px] md:w-[560px] lg:w-[1002px] */}
-                <div className="text-left w-full md:px-12 lg:px-[15px] px-[15px] font-sarala text-[16px] md:text-[28px] lg:text-[32px] md:mb-[20px] font-bold">To : </div>
+                <div className="overflow-hidden text-left w-full md:px-12 lg:px-[15px] px-[15px] font-sarala text-[16px] md:text-[28px] lg:text-[32px] md:mb-[20px] font-bold">To : </div>
                 
-                <div className="flex flex-col items-center justify-center gap-6">
+                <div className="overflow-hidden flex flex-col items-center justify-center gap-6">
                     <div className="text-left w-full md:px-12 lg:px-[15px] px-[15px] font-sarala text-[16px] md:text-[28px] lg:text-[32px] font-bold">From :</div>
                     <br/>
                     <br/>
@@ -36,13 +32,11 @@ const Message = () => {
                     <br/>
                     <br/>
                 </div>
+                
             </div>
-            
-            <img
-                src={LoveBackground}
-                alt="Hati"
-                className="fixed bottom-0 left-0 right-0 w-full  h-auto lg:h-auto z-10 object-cover"
-            />
+            <div className="absolute overflow-hidden w-full bottom-0">
+            <img src={LoveBackground} alt="Hati" className="overflow-hidden bottom-0 left-0 right-0 w-full md:scale-150 lg:scale-125 translate-y-24 h-fit z-10 object-fit" />
+            </div>
         </>
     )
 }
